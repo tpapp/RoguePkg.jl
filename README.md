66587ac06d4c35a7deb352b01df7527cc8977c5e
Tamas K. Papp <tkpapp@gmail.com>
2017-09-25 18:00:51 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # RoguePkg
 
+[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](http://www.repostatus.org/badges/latest/wip.svg)](http://www.repostatus.org/#wip)
 [![Build Status](https://travis-ci.org/tpapp/RoguePkg.jl.svg?branch=master)](https://travis-ci.org/tpapp/RoguePkg.jl)
-
 [![Coverage Status](https://coveralls.io/repos/tpapp/RoguePkg.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/tpapp/RoguePkg.jl?branch=master)
-
 [![codecov.io](http://codecov.io/github/tpapp/RoguePkg.jl/coverage.svg?branch=master)](http://codecov.io/github/tpapp/RoguePkg.jl?branch=master)
+
+This package defines some methods for functions in `Base.Pkg` that should make testing, benchmarking, and locating files in packages outside the default directory (which is returned by `Pkg.dir()`, eg `~/.julia/v0.6`) easier.
+
+It a stopgap measure, and should be obsoleted by [Pkg3](https://github.com/StefanKarpinski/Pkg3.jl).
+
+## Usage
+
+First, load the module:
+
+```julia
+using RoguePkg
+```
+
+Then you can refer to packages by the module they contain:
+
+```julia
+pkg_for"MyFancyModule"
+```
+
+A path:
+```julia
+pkg_at"~/this_is/where_I/keep/FancyPkg"
+```
+
+Or, if you set `ENV[JULIA_LOCAL_PACKAGES]`, by their subdirectory:
+```julia
+pkg_local"FancyPkg"
+```
+
+Then you can use the resulting objects with some functions, including `Pkg.test`, `Pkg.dir`. The methods for the latter even make `PkgBenchmark.benchmarkpkg` work. For example,
+
+```julia
+Pkg.test(pkg_for"FancyModule")
+```
--- src/RoguePkg.jl
+++ src/RoguePkg.jl
@@ -1,5 +1,154 @@
 module RoguePkg
 
-# package code goes here
+import Base: Pkg.Dir.path, Pkg.test, show
+
+export @pkg_for_str, @pkg_at_str, @pkg_local_str
+
+######################################################################
+# general
+######################################################################
+
+abstract type PackageSpec end
+
+"""
+    ensure_dirpath(path)
+
+If `path` is not a directory, append a `/`.
+"""
+ensure_dirpath(path) = isdirpath(path) ? path : path * "/"
+
+"""
+    package_root(package_spec)
+
+Return the root directory for a package specification.
+"""
+function pkg_root end
+
+"""
+    package_name(package_spec)
+
+Return the name of the package as a string.
+"""
+pkg_name(pkg::PackageSpec) = splitdir(splitdir(pkg_root(pkg))[1])[2]
+
+# strings are passed through
+pkg_name(s::AbstractString) = s
+
+######################################################################
+# package for a module
+######################################################################
+
+"""
+    PkgForModule(module_name)
+
+Package for the module `module_name`, located dynamically using
+`Base.find_in_path`.
+"""
+struct PkgForModule{S <: AbstractString} <: PackageSpec
+    module_name::S
+end
+
+show(io::IO, pkg::PkgForModule) =
+    print(io, "package for module $(pkg.module_name) (located dynamically)")
+
+"""
+    pkg_for"Module"
+
+Package for `"Module"`, located dynamically using `Base.find_in_path`.
+"""
+macro pkg_for_str(module_name)
+    PkgForModule(module_name)
+end
+
+function pkg_root(pkg::PkgForModule)
+    module_path = Base.find_in_path(pkg.module_name, nothing)
+    src_dir = splitdir(module_path)[1]
+    normpath(src_dir, "..")
+end
+
+######################################################################
+# package at a path
+######################################################################
+
+"""
+    PkgAtPath(path)
+
+Package at the given path. `~` is resolved.
+"""
+struct PkgAtPath{S <: AbstractString} <: PackageSpec
+    package_path::S
+end
+
+function show(io::IO, pkg::PkgAtPath)
+    path = pkg.package_path
+    print(io, "package at path $(path)")
+    '~' in path && print(io, "(~ expanded dynamically)")
+    nothing
+end
+
+"""
+    pkg_at"path"
+
+Package at the given path. `~` is resolved.
+"""
+macro pkg_at_str(package_path)
+    PackageAtPath(package_path)
+end
+
+pkg_root(pkg::PkgAtPath) = ensure_dirpath(expanduser(pkg.package_path))
+
+######################################################################
+# package in a (specified) local directory
+######################################################################
+
+const PKG_LOCAL_DIR_KEY = "JULIA_LOCAL_PACKAGES"
+
+"""
+    PkgLocal(directory)
+
+Package in `ENV[$(PKG_LOCAL_DIR_KEY)]/directory`.
+"""
+struct PkgLocal{S <: AbstractString} <: PackageSpec
+    package_name::S
+end
+
+function pkg_local_dir(expand = true)
+    haskey(ENV, PKG_LOCAL_DIR_KEY) ||
+        error("Specify local package directory with ENV[$(PKG_LOCAL_DIR_KEY)]")
+    dir = get(ENV, PKG_LOCAL_DIR_KEY)
+    expand ? expanduser(dir) : dir
+end
+
+show(io::IO, pkg::PkgLocal) =
+    print(io, "package $(pkg.package_name) in $(pkg_local_dir(false))")
+
+macro pkg_local_str(package_name)
+    PkgLocal(package_name)
+end
+
+pkg_root(pkg::PkgLocal) =
+    ensure_dirpath(normpath(pkg_local_dir(), pkg_root(pkg)))
+
+######################################################################
+# provide methods for types above
+######################################################################
+
+# when pkg is the first argument, use its root
+Pkg.Dir.path(pkg::PackageSpec, names::Union{PackageSpec, AbstractString}...) =
+    normpath(pkg_root(pkg), pkg_name.(names)...)
+
+# when pkg is not the first argument, use its name
+Pkg.Dir.path(names::Union{PackageSpec, AbstractString}...) =
+    Pkg.Dir.path(pkg_name.(names)...)
+
+# just works for a single package
+function Pkg.test(pkg::PackageSpec; coverage = false)
+    errs = AbstractString[]
+    nopkgs = AbstractString[]
+    notests = AbstractString[]
+    Pkg.Entry.test!(pkg_root(pkg), errs, nopkgs, notests; coverage = coverage)
+    # FIXME check errs, nopks, notests
+    nothing
+end
 
 end # module
