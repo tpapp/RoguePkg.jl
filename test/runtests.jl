fb1096710881ff3358e6703d9f5578f8b66c0c30
Robin Deits <robin.deits@gmail.com>
2017-11-16 11:09:48 +0000

    enable Pkg.build for rogue packages

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,21 +5,30 @@ mktempdir() do dir
     pkg_dir = joinpath(dir, "Foo")
     src_dir = joinpath(pkg_dir, "src")
     test_dir = joinpath(pkg_dir, "test")
+    deps_dir = joinpath(pkg_dir, "deps")
     mkpath(src_dir)
     mkpath(test_dir)
+    mkpath(deps_dir)
     write(joinpath(src_dir, "Foo.jl"), "module Foo\nend")
     # this will test Pkg.Entry.resolve
     write(joinpath(test_dir, "REQUIRE"), "Parameters")
     # running tests will create an empty file, which we can check
     test_path = tempname()
+    deps_path = tempname()
     write(joinpath(test_dir, "runtests.jl"), "touch(\"$(test_path)\")")
+    write(joinpath(deps_dir, "build.jl"), "touch(\"$(deps_path)\")")
+
     function all_tests(pkg)
         @test Pkg.dir(pkg, "src") == src_dir
         @test Pkg.dir("something", pkg) == Pkg.dir("something", "Foo") # NOTE hardcoded
+        Pkg.build(pkg)
+        @test isfile(deps_path)
+        rm(deps_path; force = true) # may not exist if test doesn't work, that should not error
         @test (Pkg.test(pkg); true)
         @test isfile(test_path)
         rm(test_path; force = true) # may not exist if test doesn't work, that should not error
     end
+
     # find by path
     pkg = RoguePkg.PkgAtPath(@eval @pkg_at_str($pkg_dir))
     all_tests(pkg)
